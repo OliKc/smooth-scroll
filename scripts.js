9aa6dd6bfc928b101a7c31e5bd884d9e4db95da0
OliKc <oliver.kaczmar@gmail.com>
2019-09-20 23:05:38 +0200

    navbar on scroll script

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    var scrollTop = 0;
+    $('.container').scroll(function () {
+        scrollTop = $('.container').scrollTop();
+        
+        if (scrollTop >= 200) {
+            $('.navbar').addClass('navbar-alt');
+        } else if (scrollTop < 200) {
+            $('.navbar').removeClass('navbar-alt');
+        }
+    });
+
+});
