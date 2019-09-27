e081858c1b85fa3935ea773dbb1fd5e49d5d67de
OliKc <oliver.kaczmar@gmail.com>
2019-09-28 00:28:36 +0200

    return to top button

--- scripts.js
+++ scripts.js
@@ -1,12 +1,20 @@
 $(document).ready(function () {
     var scrollTop = 0;
+    var winH = 0;
     $('.container').scroll(function () {
         scrollTop = $('.container').scrollTop();
-        
+        winH = $(window).height();
+
         if (scrollTop >= 200) {
             $('.navbar').addClass('navbar-alt');
-        } else if (scrollTop < 200) {
+        } else {
             $('.navbar').removeClass('navbar-alt');
+        }
+
+        if (scrollTop >= winH) {
+            $('.topBtn').removeClass('hidden');
+        } else {
+            $('.topBtn').addClass('hidden');
         }
     });
 });
